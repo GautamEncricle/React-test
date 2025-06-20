d75378ffe4e0fe9f333b5ad7926b45647daafa0a
Gautampatel917 <208457933+GautamEncricle@users.noreply.github.com>
2025-06-20 18:47:55 +0530

    Refactor App component to render Home component and move sections to Home for better structure and organization

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,17 @@
 import React from 'react'
 // import Home from './pages/Home'
 import Navbar from './components/Navbar'
-import ButtonWithIcon from './components/ButtonWithIcon'
 import './assets/css/font.css'
-import HeroSection from './sections/Hero'
-import AboutCompany from './sections/AboutCompany'
-import Container from './components/ServiceItem'
-import CreativeSolutions from './sections/CreativeSolutions'
-import ProjectPortfolio from './sections/ProjectPortfolio'
-import ProjectProcess from './sections/ProjectProcess'
-import ProjectDetails from './sections/ProjectsDetails'
-import TeamSection from './sections/TeamSection'
-import CollaboratorsSection from './sections/CollaboratorsSection'
-import ClientFeedback from './sections/ClientFeedback'
-import WorkShowcase from './sections/WorkShowcase'
-import SignupSection from './sections/SignupSection'
 import Footer from './components/Footer'
+import Home from './pages/Home'
 
 function App() {
 
   return (
     <>
       <Navbar />
-      <HeroSection />
-      <AboutCompany />
-      <CreativeSolutions />
-      <ProjectPortfolio />
-      <ProjectProcess />
-      <ProjectDetails />
-      <TeamSection />
-      <CollaboratorsSection />
-      <ClientFeedback />
-      <WorkShowcase />
-      <SignupSection />
-      <Footer/>
+      <Home />
+      <Footer />
     </>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import HeroSection from '../sections/Hero'
+import AboutCompany from '../sections/AboutCompany'
+import CreativeSolutions from '../sections/CreativeSolutions'
+import ProjectPortfolio from '../sections/ProjectPortfolio'
+import ProjectProcess from '../sections/ProjectProcess'
+import ProjectDetails from '../sections/ProjectsDetails'
+import TeamSection from '../sections/TeamSection'
+import CollaboratorsSection from '../sections/CollaboratorsSection'
+import ClientFeedback from '../sections/ClientFeedback'
+import WorkShowcase from '../sections/WorkShowcase'
+import SignupSection from '../sections/SignupSection'
 
 function Home() {
   return (
-    <div>Home</div>
+    <>
+      <HeroSection />
+      <AboutCompany />
+      <CreativeSolutions />
+      <ProjectPortfolio />
+      <ProjectProcess />
+      <ProjectDetails />
+      <TeamSection />
+      <CollaboratorsSection />
+      <ClientFeedback />
+      <WorkShowcase />
+      <SignupSection />
+    </>
   )
 }
 
